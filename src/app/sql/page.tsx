0c7732116cd62c07bf5d24eb3fe0d715ea8a3382
tenosh <32285955+tenosh@users.noreply.github.com>
2025-02-13 14:30:37 -0600

    Remove PromptBox from SQL page component
    - Comment out PromptBox import and component in SQL page
    - Prepare for potential future refactoring or removal of component

--- src/app/sql/page.tsx
+++ src/app/sql/page.tsx
@@ -1,14 +1,11 @@
 import { api, HydrateClient } from "~/trpc/server";
-import { PromptBox } from "../_components/PromptBox";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
   return (
     <HydrateClient>
-      <div className="container mx-auto py-8">
-        <PromptBox />
-      </div>
+      <div className="container mx-auto py-8">{/* <PromptBox /> */}</div>
     </HydrateClient>
   );
 }
